b20f6ac5cd067f15e9ebb28fdfea302dddf765b5
Storm <Storm.v.laursen@gmail.com>
2025-11-03 12:13:57 +0100

    kort opdateret til et fast punkt og stadions tilføjet

--- sketch.js
+++ sketch.js
@@ -1,17 +1,66 @@
 let img;
+let stadions;
+let screenWidth = 1280;
+let screenHeight = 550;
+
+class Stadion {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  display() {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(this.x, this.y, 10, 10);
+  }
+}
 
 function preload() {
-  img = loadImage('danmarkskort.png');
+  img = loadImage("danmarkskort.png");
+}
+
+function drawMap() {
+  imageMode(CENTER);
+  if (img) {
+    image(
+      img,
+      screenWidth / 2,
+      screenHeight / 2,
+      (860 / 1071) * screenHeight,
+      screenHeight
+    );
+  }
+}
+
+function drawStadions() {
+  for (let stadion of stadions) {
+    stadion.display();
+  }
+}
+
+function drawLinesBetweenStadions() {
+  stroke(0, 0, 255);
+  strokeWeight(2);
+  for (let i = 0; i < stadions.length; i++) {
+    for (let j = i + 1; j < stadions.length; j++) {
+      line(stadions[i].x, stadions[i].y, stadions[j].x, stadions[j].y);
+    }
+  }
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
+  createCanvas(screenWidth, screenHeight);
+  stadions = [
+    new Stadion(600, 250),
+    new Stadion(630, 380),
+    new Stadion(820, 330),
+  ];
 }
 
 function draw() {
   background(220);
-  if (img) {
-    image(img, windowWidth/2, windowHeight/2, 860/1071*windowHeight, windowHeight);
-  }
+  drawMap();
+  drawStadions();
+  drawLinesBetweenStadions();
 }
